4d8ccf01ad39f8113f749e5e6d2b06163cc16bfa
ltrgordon <ltrgordon@gmail.com>
2025-08-23 07:42:38 -0700

    Revert "Refactor jump-kids into modular structure"
    This reverts commit 98cefa6b08310d4221a9985a966389b5a84e270b.

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-import js from '@eslint/js';
-
-export default [
-  js.configs.recommended,
-  {
-    languageOptions: { sourceType: 'module' },
-  },
-];
--- jump-kids/rendering.js
+++
@@ -1,9 +0,0 @@
-import { TILE, COL } from './config.js';
-
-export function render(ctx, player) {
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  ctx.fillStyle = COL.ground;
-  ctx.fillRect(0, ctx.canvas.height - TILE, ctx.canvas.width, TILE);
-  ctx.fillStyle = '#ff0000';
-  ctx.fillRect(player.x, player.y - player.h, player.w, player.h);
-}
